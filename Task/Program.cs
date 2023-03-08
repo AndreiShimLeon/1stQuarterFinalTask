fc8bf1758d7eeadbc899aa97a534e55afa6b6015
AndreiShimLeon <art.shim@yandex.ru>
2023-03-08 21:37:01 +0300

    creation of the array added

---
+++ Task/Program.cs
@@ -0,0 +1,32 @@
+﻿// Убрать из массива все элементы с длиной больше 3 символа.
+
+int ReadInt(string text)
+{
+    Console.WriteLine(text);
+    return int.Parse(Console.ReadLine()!);
+}
+
+string ReadString(string text)
+{
+    Console.WriteLine(text);
+    return Console.ReadLine()!;
+}
+
+string[] ReadStringArray(int length)
+{
+    string[] array = new string[length];
+
+    for (int i = 0; i < length; i++)
+        array[i] = ReadString($"Enter element #{i + 1}: ");
+    return array;
+}
+
+void PrintStrArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"'{array[i]}' ");
+    }
+}
+
+int length = ReadInt("Enter array size: ");
